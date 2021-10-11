1d9d308c55d86fb82f1c99b261f3f0f4443370b3
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-10-11 15:57:58 -0300

    First Checkpoint

---
+++ CheckPoint_1.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+// Criar uma constante de qualquer temperatura em Celsius.
+
+let celsius: Double = 23.0
+
+// Converte-o em Fahrenheit multiplicando por 9 e devidindo por 5 e somando 32
+
+let fahrenheit: Double = 23 * 9 / 5 + 32
+
+// Imprima o resultado para o usuário e mostre o valor de Celsius e Fahrenheit
+print("O valor em Celsius é: \(celsius) e o valor em Fahrenheit é \(fahrenheit)")
