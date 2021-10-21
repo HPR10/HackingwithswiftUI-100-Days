04c6115483199e685b7e61373ce800ca3fe2a26f
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-10-21 00:02:54 -0300

    loops, summary

--- Day6.playground/Contents.swift
+++ Day6.playground/Contents.swift
@@ -1,7 +1,6 @@
 import Cocoa
 
-// MARK: - LOOPS
-
+// MARK: - FOR LOOP
 var plataformas = ["MacOS", "tvOS", "iOS", "watchOS"]
 
 for os in plataformas {
@@ -16,7 +15,6 @@ for i in 1...10 {
     }
 }
 
-// código sem sentido
 var nome = "Hugo"
 for _ in 1..<2 {
     nome += " Pinheiro"
@@ -28,5 +26,56 @@ for _ in 1..<2 {
 print(nome)
 
 
+for i in 1...5 {
+    print("Contando de 1 até 5: \(i)")
+}
 
 
+var letra = "Juro café da manhã preparar"
+for _ in 1...5 {
+    letra += " Consigo um outro nó com alguém"
+}
+print(letra)
+
+
+// MARK: - WHILE
+var contador = 10
+while contador > 0 {
+    print("\(contador)..")
+    contador = contador - 1
+}
+print("Blast Off")
+
+
+var dado = 0
+while dado != 20 {
+    dado = Int.random(in: 1...20)
+    print("O Valor do dado nessa rodada é \(dado)")
+}
+print("Ataque crítico")
+
+
+// Pulando itens no loop
+
+let arquivos = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+for arquivo in arquivos {
+    if arquivo.hasSuffix(".jpg") == false {
+        continue
+    }
+    
+    print("Arquivo Encontrado: \(arquivo)")
+}
+
+let numeroUm = 3
+let numeroDois = 10
+var multiplos = [Int]()
+for i in 1...100_000 {
+    if i.isMultiple(of: numeroUm) && i.isMultiple(of: numeroDois) {
+        multiplos.append(i)
+        
+        if multiplos.count == 10 {
+            break
+        }
+    }
+}
+print(multiplos)
