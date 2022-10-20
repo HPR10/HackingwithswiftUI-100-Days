303bbc3a52124f1ac5ecb31ef39e3dcde18ab782
Hugo Pinheiro <hugo_hp10@hotmail.com>
2022-10-19 22:38:48 -0300

    Double and Int Revision

---
+++ Day15.playground/Pages/Inteiros.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+// Swift armazena inteiros usando o tipo Int e ele suporta várias operações matemáticas
+let score = 10
+let highScore = score + 10
+let halvedScorem = score / 2
+
+// Suporta operadores de atribuição
+var counter = 10
+counter += 5
+
+// Inteiros contém alguns métodos já inbutido como: ismultiple e random
+let number = 120
+print(number.isMultiple(of: 3))
+
+let id = Int.random(in: 1...10)
+
---
+++ Day15.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+// MARK: - Strings
+
+// Strings começam e terminam com "".
+let actor = "Hugo Pinheiro"
+
+// Strings aceitam emojs 🥰.
+let pet = "Jao Pinheiro 🐶"
+
+// Colocar aspas na String
+let frase = "Eu sou ser um grande \"Desenvolvedor\" da platlaforma ios"
+
+// váriáveis ou constantes em mais de uma linha.
+var filmes = """
+Harry potter
+e o cálice de fogo
+"""
+
+// MARK: - Métodos para Strings
+
+// Count: Método de contar quantas letras tem em uma string.
+print(filmes.count)
+
+// hasPrefix and hasSuffix: Verifica se uma string começa ou termina com letras específicas com retorno bolleano.
+print(actor.hasPrefix("Hu"))
+print(pet.hasSuffix("🐶"))
+
+
