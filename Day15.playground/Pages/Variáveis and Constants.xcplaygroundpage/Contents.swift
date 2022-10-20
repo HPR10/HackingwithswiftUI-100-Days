8136b5ba6fd4d294b48b96d11c2c772d7923e6d9
Hugo Pinheiro <hugo_hp10@hotmail.com>
2022-10-19 23:00:03 -0300

    Boolean and Double Revision

---
+++ Day15.playground/Pages/Variáveis and Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+// MARK: - Variáveis e constantes
+
+var name = "Hugo"
+let lastName = "Pinheiro"
+
+// Modificando variáveis e constantes
+name = "Raimundo"
+// lastName = "Raimundo" - Constante não pode ser alterada.
+
+print(name) // Imprimindo no console o valor de name
